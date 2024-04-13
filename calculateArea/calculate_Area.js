f3b3f6678cf27c2967a99723ebe3a480bbdbe4c3
Rahim Akkoyun <rahim.akkoyun@outlook.com>
2024-04-13 15:41:08 +0200

    Writing Your First Javascript Function Lab complete

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,20 @@
+let length;
+let width;
+
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+   
+   let area = length * width;
+   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+   }
+
+function calculateTotalAmount()
+{
+    amount1 = parseFloat(document.getElementById('amount1').value);
+    amount2 = parseFloat(document.getElementById('amount2').value);
+    amount3 = parseFloat(document.getElementById('amount3').value);
+
+    let totalAmount = amount1 + amount2 + amount3;
+    document.getElementById('totalAmount').innerText = `The total amount is: $ ${totalAmount}`;
+}
